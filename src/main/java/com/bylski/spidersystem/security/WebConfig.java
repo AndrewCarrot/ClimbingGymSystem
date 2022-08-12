7711a9ed9cceac56f1534bd9955a16642a7060b0
AndrewCarrot <marbyl002@utp.edu.pl>
2022-08-12 12:21:15 +0200

    cors

---
+++ src/main/java/com/bylski/spidersystem/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bylski.spidersystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * enables CORS requests from any origin to any endpoint in the application.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
